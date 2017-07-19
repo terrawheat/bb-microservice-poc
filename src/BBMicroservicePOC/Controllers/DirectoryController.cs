1cb5a8166e353927b4da6a4961aad9970a427849
Matt Wheatley <matthew.wheatley@liberis.co.uk>
2017-07-19 11:21:14 +0100

    Initial commit

---
+++ src/BBMicroservicePOC/Controllers/DirectoryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BBMicroservicePOC.Controllers
+{
+    [Route("api/[controller]")]
+    public class DirectoryController : Controller
+    {
+        [HttpGet]
+        public IDictionary<ServiceType, IEnumerable<Uri>> Get()
+        {
+            return new Dictionary<ServiceType, IEnumerable<Uri>>()
+            {
+                { ServiceType.SampleService, new List<Uri>()
+                {
+                    new Uri("localhost:29777")
+                } }
+            };
+        }
+
+    }
+
+    public enum ServiceType
+    {
+        SampleService = 1
+    }
+}
